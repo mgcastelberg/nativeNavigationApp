9f250048cef1f6a2ae880eff205a9f3f21d397ad
Manuel Gomez <jmgc@virket.com>
2024-04-13 23:37:36 -0600

    react navigation config and intallation of navigation  stack

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native'
 import React from 'react'
 import { Text, View } from 'react-native'
+import { StackNavigator } from './presentation/routes/StackNavigator';
 
 export const App = () => {
   return (
-    <View>
-      <Text>Hola Mundo</Text>
-    </View>
+    <NavigationContainer>
+      <StackNavigator/>
+    </NavigationContainer>
   )
 }
