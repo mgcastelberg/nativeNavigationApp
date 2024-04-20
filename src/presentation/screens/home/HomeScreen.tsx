337d614e0a2c845bc5c895d0ef88416567673193
Manuel Gomez <jmgc@virket.com>
2024-04-20 11:32:56 -0600

    Enviar argumentos entre pantallas

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import { Pressable, Text, View } from 'react-native'
 import { globalStyles } from '../../theme/theme';
-import { useNavigation } from '@react-navigation/native';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { PrimaryButton } from '../../components/shared/PrimaryButton';
+import { RootStackParams } from '../../routes/StackNavigator';
 
 export const HomeScreen = () => {
 
-  const navigation = useNavigation();
+  const navigation = useNavigation<NavigationProp<RootStackParams>>();
 
   return (
     <View style={ globalStyles.container }>
-      <PrimaryButton label="Productos" onPress={ () => navigation.navigate('Products' as never) } />
-      <PrimaryButton label="Settings" onPress={ () => navigation.navigate('Settings' as never) } />
+      <PrimaryButton label="Productos" onPress={ () => navigation.navigate('Products') } />
+      <PrimaryButton label="Settings" onPress={ () => navigation.navigate('Settings') } />
 
-      <Pressable onPress={ () => navigation.navigate('Products' as never) } style={ globalStyles.primaryButton } >
+      <Pressable onPress={ () => navigation.navigate('Products') } style={ globalStyles.primaryButton } >
           <Text style={ globalStyles.buttonText } >Productos</Text>
       </Pressable>
     </View>
--- src/presentation/screens/products/ProductScreen.tsx
+++ src/presentation/screens/products/ProductScreen.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native'
+import React, { useEffect } from 'react'
 import { Text, View } from 'react-native'
+import { RootStackParams } from '../../routes/StackNavigator'
+import { globalStyles } from '../../theme/theme'
 
 export const ProductScreen = () => {
+
+  const params = useRoute<RouteProp<RootStackParams, 'Product'>>().params;
+  // console.log(params);
+
+  const navigation = useNavigation();
+  // para que se ejecute una sola vez cuando se inicia el componente
+  useEffect(() => {
+    navigation.setOptions({
+      title: params.name
+    });
+  }, []);
+
   return (
-    <View>
+    <View style={ globalStyles.container }>
         <Text>ProductScreen</Text>
+        <Text style={{ fontSize:20, textAlign: 'center', marginTop:20 }} >{ params?.id } - { params?.name }</Text>
     </View>
   )
 }
