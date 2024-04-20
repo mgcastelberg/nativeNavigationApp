9df707a1b4ad9beb2ac1edb1c7a1c19eb8f918ab
Manuel Gomez <jmgc@virket.com>
2024-04-19 18:34:46 -0600

    Navegar a otras pantallas

---
+++ src/presentation/components/shared/PrimaryButton.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Pressable, Text } from 'react-native'
+import { globalStyles } from '../../theme/theme';
+
+interface Props {
+    label: string;
+    onPress: () => void
+}
+
+export const PrimaryButton = ( { label, onPress}:Props ) => {
+    return (
+        <Pressable onPress={ () => onPress() } style={ globalStyles.primaryButton } >
+            <Text style={ globalStyles.buttonText } >{ label }</Text>
+        </Pressable>
+    )
+}
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Pressable, Text, View } from 'react-native'
+import { globalStyles } from '../../theme/theme';
+import { useNavigation } from '@react-navigation/native';
+import { PrimaryButton } from '../../components/shared/PrimaryButton';
 
 export const HomeScreen = () => {
+
+  const navigation = useNavigation();
+
   return (
-    <View>
-        <Text>HomeScreen</Text>
+    <View style={ globalStyles.container }>
+      <PrimaryButton label="Productos" onPress={ () => navigation.navigate('Products' as never) } />
+      <PrimaryButton label="Settings" onPress={ () => navigation.navigate('Settings' as never) } />
+
+      <Pressable onPress={ () => navigation.navigate('Products' as never) } style={ globalStyles.primaryButton } >
+          <Text style={ globalStyles.buttonText } >Productos</Text>
+      </Pressable>
     </View>
   )
 }
