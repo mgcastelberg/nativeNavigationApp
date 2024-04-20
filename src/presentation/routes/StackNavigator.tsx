95da72037b3b7bb09bee046d562ff68fd8e70a64
Manuel Gomez <jmgc@virket.com>
2024-04-19 18:45:59 -0600

    Estilizando el Stack Navigator

--- src/presentation/routes/StackNavigator.tsx
+++ src/presentation/routes/StackNavigator.tsx
@@ -7,8 +7,14 @@ const Stack = createStackNavigator();
 
 export const StackNavigator = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
+    <Stack.Navigator screenOptions={{
+      headerShown:true,
+      headerStyle:{
+        elevation: 0,
+        shadowColor: 'transárent'
+      }
+    }}>
+      <Stack.Screen options={{  }} name="Home" component={HomeScreen} />
       <Stack.Screen name="Products" component={ProductsScreen} />
       <Stack.Screen name="Settings" component={SettingsScreen} />
     </Stack.Navigator>
