529cee51b83669f4ec8a9fbcaa27b5cbf1173c88
Manuel Gomez <jmgc@virket.com>
2024-04-21 21:25:38 -0600

    Configurar drawer basico

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { NavigationContainer } from '@react-navigation/native'
 import React from 'react'
 import { Text, View } from 'react-native'
 import { StackNavigator } from './presentation/routes/StackNavigator';
+import { SideMenuNavigator } from './presentation/routes/SideMenuNavigator';
 
 export const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator/>
+      {/* <StackNavigator/> */}
+      <SideMenuNavigator/>
     </NavigationContainer>
   )
 }
---
+++ src/presentation/screens/profile/ProfileScreen.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native'
+import { globalStyles } from '../../theme/theme'
+
+export const ProfileScreen = () => {
+  return (
+    <View style={globalStyles.container}>
+        <Text>ProfileScreen</Text>
+    </View>
+  )
+}
