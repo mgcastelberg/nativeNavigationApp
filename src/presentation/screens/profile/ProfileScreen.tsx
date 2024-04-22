141c8426060ada2b6e5c8f8ed73d1c8942382881
Manuel Gomez <jmgc@virket.com>
2024-04-22 00:15:12 -0600

    determina el useSafeAreaInserts

--- src/presentation/screens/profile/ProfileScreen.tsx
+++ src/presentation/screens/profile/ProfileScreen.tsx
@@ -1,10 +1,21 @@
 import { Text, View } from 'react-native'
-import { globalStyles } from '../../theme/theme'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { PrimaryButton } from '../../components/shared/PrimaryButton';
+import { DrawerActions, useNavigation } from '@react-navigation/native';
 
 export const ProfileScreen = () => {
+
+  const { top } = useSafeAreaInsets();
+  const navigation = useNavigation();
+
   return (
-    <View style={globalStyles.container}>
-        <Text>ProfileScreen</Text>
+    <View style={{
+      flex:1,
+      paddingHorizontal: 20,
+      marginTop: top + 20
+    }}>
+        <Text style={{ marginBottom:10 }} >ProfileScreen</Text>
+        <PrimaryButton label='Abrir Menu' onPress={ () => navigation.dispatch( DrawerActions.toggleDrawer )}/>
     </View>
   )
 }
