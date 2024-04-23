47ced43c28e6958df5386b48719b9c61a33a2d0c
Manuel Gomez <jmgc@virket.com>
2024-04-22 23:15:26 -0600

    Personalizando el BottomTabNavigator

--- src/presentation/routes/BottomTabsNavigator.tsx
+++ src/presentation/routes/BottomTabsNavigator.tsx
@@ -2,15 +2,37 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Tab1Screen } from '../screens/tabs/Tab1Screen';
 import { Tab2Screen } from '../screens/tabs/Tab2Screen';
 import { Tab3Screen } from '../screens/tabs/Tab3Screen';
+import { globalColors } from '../theme/theme';
+import { Text } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
 export const BottomTabNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Tab1" component={ Tab1Screen } />
-      <Tab.Screen name="Tab2" component={ Tab2Screen } />
-      <Tab.Screen name="Tab3" component={ Tab3Screen } />
+    <Tab.Navigator
+      sceneContainerStyle={{
+        backgroundColor:globalColors.background
+      }}
+      screenOptions={{
+        // headerShown:false
+        tabBarLabelStyle:{
+          marginBottom:5,
+          // display:'none'
+        },
+        headerStyle:{
+          elevation:0, // no se muestre linea header en Android
+          borderColor:'transparent',
+          shadowColor:'transparent'
+        },
+        tabBarStyle:{
+          borderTopWidth: 0, // no se muestre linea header en Ios
+          elevation:0
+        }
+      }}
+    >
+      <Tab.Screen name="Tab1" options={{ title:'Tab 1', tabBarIcon: (props) => (<Text style={{ color: props.color }}>Tab 1</Text>) }} component={ Tab1Screen } />
+      <Tab.Screen name="Tab2" options={{ title:'Tab 2', tabBarIcon: (props) => (<Text style={{ color: props.color }}>Tab 2</Text>) }} component={ Tab2Screen } />
+      <Tab.Screen name="Tab3" options={{ title:'Tab 3', tabBarIcon: (props) => (<Text style={{ color: props.color }}>Tab 3</Text>) }} component={ Tab3Screen } />
     </Tab.Navigator>
   );
 }
