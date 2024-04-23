bbb40c9f6f4058fea91f71090f42ed77d3822866
Manuel Gomez <jmgc@virket.com>
2024-04-22 22:46:47 -0600

    Creando y configurando el BottomTabNavigator

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import React from 'react'
 import { Text, View } from 'react-native'
 import { StackNavigator } from './presentation/routes/StackNavigator';
 import { SideMenuNavigator } from './presentation/routes/SideMenuNavigator';
+import { BottomTabNavigator } from './presentation/routes/BottomTabsNavigator';
 
 export const App = () => {
   return (
     <NavigationContainer>
       {/* <StackNavigator/> */}
       <SideMenuNavigator/>
+      {/* <BottomTabNavigator/> */}
     </NavigationContainer>
   )
 }
