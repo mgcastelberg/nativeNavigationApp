eb5491b40b9cd0f7df2156ec246e8fbda34f371c
Manuel Gomez <jmgc@virket.com>
2024-04-25 00:05:22 -0600

    Iconos - Instalaciones

--- src/presentation/screens/tabs/Tab1Screen.tsx
+++ src/presentation/screens/tabs/Tab1Screen.tsx
@@ -2,12 +2,15 @@ import { DrawerActions, useNavigation } from '@react-navigation/native'
 import { View, Text } from 'react-native'
 import { HamburgerMenu } from '../../components/shared/HamburgerMenu';
 
+import Icon from 'react-native-vector-icons/Ionicons';
+
 export const Tab1Screen = () => {
 
   return (
     <View>
         <HamburgerMenu />
         <Text>Tab1Screen</Text>
+        <Icon name="rocket-outline" size={70} color="#900" />
     </View>
   )
 }
